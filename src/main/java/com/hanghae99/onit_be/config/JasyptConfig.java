60d55c49c0fe68be5d725bdc559ff786df7661fc
zsoon96 <pres0616@gmail.com>
2022-05-24 06:34:25 +0900

    CICD 암호화

--- src/main/java/com/hanghae99/onit_be/config/JasyptConfig.java
+++ src/main/java/com/hanghae99/onit_be/config/JasyptConfig.java
@@ -1,33 +1,33 @@
-//package com.hanghae99.onit_be.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.jasypt.encryption.StringEncryptor;
-//import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-//import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-//
-//@Configuration
-//public class JasyptConfig {
-//
-//    @Value("${jasypt.encryptor.password}")
-//    private String encryptKey;
-//
-//    @Bean("jasyptStringEncryptor")
-//    public StringEncryptor stringEncryptor() {
-//        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-//        System.out.println("encryptKey : " + encryptKey);
-//        config.setPassword(encryptKey);
-////        config.setPassword(System.getenv("JASYPT_PASSWORD")); // 환경 변수
-//        config.setAlgorithm("PBEWithMD5AndDES");
-//        config.setKeyObtentionIterations("1000");
-//        config.setPoolSize("1");
-//        config.setProviderName("SunJCE");
-//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-//        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
-//        config.setStringOutputType("base64");
-//        encryptor.setConfig(config);
-//        return encryptor;
-//    }
-//}
+package com.hanghae99.onit_be.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String encryptKey;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        System.out.println("encryptKey : " + encryptKey);
+        config.setPassword(encryptKey);
+//        config.setPassword(System.getenv("JASYPT_PASSWORD")); // 환경 변수
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
