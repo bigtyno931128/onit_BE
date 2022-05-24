68aa5087f82a3dd48a8db9e15fbf62b093f9bb54
zsoon96 <pres0616@gmail.com>
2022-05-24 09:08:33 +0900

    CICD 원복

--- src/main/java/com/hanghae99/onit_be/config/AmazonS3Config.java
+++ src/main/java/com/hanghae99/onit_be/config/AmazonS3Config.java
@@ -24,7 +24,7 @@ public class AmazonS3Config {
 
     @Bean
     public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
